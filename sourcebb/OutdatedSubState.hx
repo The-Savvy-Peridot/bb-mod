dc4a0d6435ba8c56f6364ab5457e37d8ba82735e
slameron <cameron.luebbe@gmail.com>
2021-06-23 15:40:27 -0400

    Desktop Fullscreen Message
    awesome code

--- sourcebb/OutdatedSubState.hx
+++ sourcebb/OutdatedSubState.hx
@@ -9,6 +9,40 @@ import lime.app.Application;
 
 class OutdatedSubState extends MusicBeatState
 {
+	#if html5
 	public static var leftState:Bool = true;
+	#end
+	#if desktop
+	public static var leftState:Bool = false;
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"HEY!\nThis mod runs in fullscreen by default."
+			+ "\nif you want to play in windowed, press ALT+ENTER"
+			+ "\nor you can use ALT+F4 to close the game."
+			+ "\nClose this menu with Space or Escape.",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
 
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+	#end
 }
